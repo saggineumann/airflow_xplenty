11273e9571a9eee5c5af04ff8115e102e1eb05d2
Tom Collier <collier@apartmentlist.com>
2018-04-18 13:41:39 -0700

    style: split multiple statements
    Fixes pylint error: "More than one statement on a single
    line (multiple-statements)"
    
    pylint tests
    Your code has been rated at 7.14/10 (previous run: 6.67/10, +0.48)

--- tests/airflow_xplenty/test_client_factory.py
+++ tests/airflow_xplenty/test_client_factory.py
@@ -6,7 +6,8 @@ from airflow_xplenty.client_factory import ClientFactory, XplentyV2Client
 class ClientFactoryTestCase(unittest.TestCase):
     def setUp(self):
         # TODO: set these values in unittest.cfg and have Airflow read from there
-        if not conf.has_section('xplenty'): conf.add_section('xplenty')
+        if not conf.has_section('xplenty'):
+            conf.add_section('xplenty')
         conf.set('xplenty', 'account_id', 'TestAccount')
         conf.set('xplenty', 'api_key', 'TestKey')
 
