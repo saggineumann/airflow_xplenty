569ce71b4ba786e91b2b9713f351ec0b69c50819
Tom Collier <collier@apartmentlist.com>
2018-04-18 14:05:45 -0700

    style: remove spaces around kwarg equals
    Fixes flake8 error: 'E251 unexpected spaces around keyword / parameter equals'

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
 from distutils.core import setup
 from airflow_xplenty.version import VERSION
 
+
 def do_setup():
     setup(
-      name = 'airflow_xplenty',
-      packages = ['airflow_xplenty', 'airflow_xplenty.operators'],
-      version = VERSION,
-      description = 'Airflow wrappers for the Xplenty API',
-      author = 'Tom Collier',
-      author_email = 'collier@apartmentlist.com',
-      url = 'https://github.com/apartmentlist/airflow_xplenty',
-      download_url =
-        ('https://github.com/apartmentlist/airflow_xplenty/archive/' + VERSION + '.tar.gz'),
-      keywords = ['airflow', 'xplenty'],
-      install_requires = ['apache-airflow (>= 1.8.2)', 'xplenty (>= 1.1.2)'],
-      classifiers = []
+      name='airflow_xplenty',
+      packages=['airflow_xplenty', 'airflow_xplenty.operators'],
+      version=VERSION,
+      description='Airflow wrappers for the Xplenty API',
+      author='Tom Collier',
+      author_email='collier@apartmentlist.com',
+      url='https://github.com/apartmentlist/airflow_xplenty',
+      download_url=(
+        'https://github.com/apartmentlist/airflow_xplenty/archive/' + VERSION +
+        '.tar.gz'),
+      keywords=['airflow', 'xplenty'],
+      install_requires=['apache-airflow (>= 1.8.2)', 'xplenty (>= 1.1.2)'],
+      classifiers=[]
     )
+
 
 if __name__ == "__main__":
     do_setup()
