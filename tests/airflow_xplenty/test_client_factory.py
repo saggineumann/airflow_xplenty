e541a7c5d50db239359c4e57ce32095466663e4b
Tom Collier <collier@apartmentlist.com>
2018-05-10 10:27:24 -0700

    Add specs for ClientFactory#get_job

--- tests/airflow_xplenty/test_client_factory.py
+++ tests/airflow_xplenty/test_client_factory.py
@@ -1,6 +1,8 @@
 import unittest
+from mock import MagicMock
 from airflow.configuration import conf
 from airflow_xplenty.client_factory import ClientFactory, XplentyV2Client
+from xplenty import Job
 
 
 class ClientFactoryTestCase(unittest.TestCase):
@@ -9,18 +11,21 @@ class ClientFactoryTestCase(unittest.TestCase):
             conf.add_section('xplenty')
         conf.set('xplenty', 'account_id', 'TestAccount')
         conf.set('xplenty', 'api_key', 'TestKey')
+        self.client = ClientFactory().client()
 
     def test_client_type(self):
-        factory = ClientFactory()
-        client = factory.client()
-        self.assertEqual(type(client), XplentyV2Client)
+        self.assertEqual(type(self.client), XplentyV2Client)
 
     def test_client_account_id(self):
-        factory = ClientFactory()
-        client = factory.client()
-        self.assertEqual(client.account_id, 'TestAccount')
+        self.assertEqual(self.client.account_id, 'TestAccount')
 
     def test_client_api_key(self):
-        factory = ClientFactory()
-        client = factory.client()
-        self.assertEqual(client.api_key, 'TestKey')
+        self.assertEqual(self.client.api_key, 'TestKey')
+
+    def test_get_job(self):
+        self.client.get = MagicMock(return_value={'outputs': 'boom'})
+        job = self.client.get_job(34)
+        self.client.get.assert_called_with(
+            'https://api.xplenty.com/TestAccount/api/jobs/34')
+        self.assertEqual(Job, type(job))
+        self.assertEqual('boom', job.outputs)
